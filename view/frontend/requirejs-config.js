d9c8268c8984843ad2de1c5cbc2ffd89a9a5029b
dipika.softnoesis <dipika.softnoesis@gmail.com>
2022-09-12 18:49:15 +0530

    add Related-Products extension

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+        "owl.carousel": "Softnoesis_RelatedProduct/js/owl.carousel",
+    },
+    shim: {
+        "owl.carousel": {
+            deps: ["jquery"],
+        },
+    },
+};
